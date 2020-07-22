120c49c4e8a6f8350b4100774b82d33367b82910
horlamiedea <horlamiedea@gmail.com>
2020-07-22 10:16:57 +0100

    changed debug to false

--- my_port/settings.py
+++ my_port/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q!bzcnpv+7tl36_$=j@l6j755(o*05f9t%3o$pui4o#pcrsoaz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
